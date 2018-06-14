85d0d637dd5fb576629ec7aa21531da46803be03
DESKTOP-L79ED6V\Omer58 <of.c.58@hotmail.com>
2018-06-14 22:34:37 +0200

    VIDEO 297 - GOOD

--- Source/S05_TestingGrounds/S05_TestingGroundsGameMode.cpp
+++ Source/S05_TestingGrounds/S05_TestingGroundsGameMode.cpp
@@ -9,7 +9,7 @@ AS05_TestingGroundsGameMode::AS05_TestingGroundsGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Player/CharacterBlueprints/FirstPersonCharacter_BP"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/NPC/CharacterBlueprint/Ue4ASP_Character"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
